5336cd9f9ec52a7539784a80f3b89666706f66f9
JoGaDoR <thiagojogador171@gmail.com>
2023-11-05 02:50:09 -0300

    feat: global css

---
+++ module02_application_structure/02_global_css/src/App.jsx
@@ -0,0 +1,23 @@
+import { Post } from "./Post.jsx";
+import { Header } from "./components/Header.jsx";
+
+import "./styles.css"
+
+export function App() {
+  return (
+    <div>
+      <Header/>
+
+      <Post 
+        author="Thiago" 
+        content="lorem ipsum dolor sit amet" 
+      />
+      <Post 
+        author="Truta"
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, consectetur enim recusandae magnam veritat"
+      />
+    </div>
+  );
+}
+
+// export default App
