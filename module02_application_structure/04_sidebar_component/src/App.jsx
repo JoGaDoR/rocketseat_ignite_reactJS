afe673c0aa60ab56075ebddd31fe84c3044edfd9
JoGaDoR <thiagojogador171@gmail.com>
2023-11-08 20:22:24 -0300

    feat: refactoring code

--- module02_application_structure/04_sidebar_component/src/App.jsx
+++ module02_application_structure/04_sidebar_component/src/App.jsx
@@ -1,21 +1,26 @@
 import { Post } from "./Post.jsx";
 import { Header } from "./components/Header.jsx";
 
-import "./global.css"
+import styleApp from "./App.module.css";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
-      <Header/>
+      <Header />
 
-      <Post 
-        author="Thiago" 
-        content="lorem ipsum dolor sit amet" 
-      />
-      <Post 
-        author="Truta"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, consectetur enim recusandae magnam veritat"
-      />
+      <div className={styleApp.wrapper}></div>
+      <aside>
+        siderbar
+      </aside>
+      <main>
+        <Post author="Thiago" content="lorem ipsum dolor sit amet" />
+        <Post
+          author="Truta"
+          content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, consectetur enim recusandae magnam veritat"
+        />
+      </main>
     </div>
   );
 }
