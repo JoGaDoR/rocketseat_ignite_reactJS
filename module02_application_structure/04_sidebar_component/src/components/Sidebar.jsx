8af630e40a561999ff344dbfc8c4c9f31fff5af7
JoGaDoR <thiagojogador171@gmail.com>
2023-11-08 20:43:21 -0300

    feat: sidebar

--- module02_application_structure/04_sidebar_component/src/components/Sidebar.jsx
+++ module02_application_structure/04_sidebar_component/src/components/Sidebar.jsx
@@ -2,6 +2,19 @@ import styleSidebar from "./Sidebar.module.css"
 
 export function Sidebar(){
     return (
-        <aside className={styleSidebar.sidebar}></aside>
+        <aside className={styleSidebar.sidebar}>
+            <img src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
+            <div className={styleSidebar.profile}>
+                <strong>UserName</strong>
+                <span>CARGO</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
     );
 }
