fb74b2994899ad821d747403864c40dde04f13d6
JoGaDoR <thiagojogador171@gmail.com>
2023-11-08 20:48:39 -0300

    feat: style cover

--- module02_application_structure/04_sidebar_component/src/components/Sidebar.jsx
+++ module02_application_structure/04_sidebar_component/src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import styleSidebar from "./Sidebar.module.css"
 export function Sidebar(){
     return (
         <aside className={styleSidebar.sidebar}>
-            <img src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <img className={styleSidebar.cover} src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styleSidebar.profile}>
                 <strong>UserName</strong>
@@ -12,7 +12,7 @@ export function Sidebar(){
 
             <footer>
                 <a href="#">
-                    Editar seu
+                    Editar seu perfil
                 </a>
             </footer>
         </aside>
