aea566912abd0fce9c53889882a857694f4b2cc4
JoGaDoR <thiagojogador171@gmail.com>
2023-11-08 21:43:32 -0300

    feat: avatar

--- module02_application_structure/04_sidebar_component/src/components/Sidebar.jsx
+++ module02_application_structure/04_sidebar_component/src/components/Sidebar.jsx
@@ -6,6 +6,8 @@ export function Sidebar(){
             <img className={styleSidebar.cover} src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styleSidebar.profile}>
+                <img className={styleSidebar.avatar} src="https://github.com/jogador.png" />
+                
                 <strong>UserName</strong>
                 <span>CARGO</span>
             </div>
