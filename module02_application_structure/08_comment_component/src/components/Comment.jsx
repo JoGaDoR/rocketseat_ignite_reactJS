91204ae50e40204299413700f8f93d78b0241965
JoGaDoR <thiagojogador171@gmail.com>
2023-11-16 02:10:49 -0300

    feat: comment strutcture

--- module02_application_structure/08_comment_component/src/components/Comment.jsx
+++ module02_application_structure/08_comment_component/src/components/Comment.jsx
@@ -7,11 +7,26 @@ export function Comment(){
 
             <div className={styleComment.commentBox}>
                 <div className={styleComment.commentContent}>
-                    
+                    <header>
+                        <div className={styleComment.authorAndTime}>
+                            <strong>Thiago Coelho</strong>
+                            <time title="11 de maio as 9:13" dateTime="2022-05-11">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!</p>
+
                 </div>
 
                 <footer>
-                    Aplaudir
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
                 </footer>
             </div>
         </div>
