b678d619b39524f4e066cda1098a7ff0e0ac36c1
JoGaDoR <thiagojogador171@gmail.com>
2023-11-17 19:38:06 -0300

    feat: iterating in jsx

---
+++ module03_react_engines/01_iterating_jsx/src/components/Avatar.jsx
@@ -0,0 +1,23 @@
+import styleAvatar from "./Avatar.module.css"
+
+// export function Avatar(props){
+//     const hasBorder = props.hasBorder !== false;
+
+//     return (
+//         <img 
+//             className={hasBorder ? styleAvatar.avatarWithBorder : styleAvatar.avatar} 
+//             src="props.src" 
+//         />
+//     );
+// }
+
+export function Avatar({ hasBorder = true, src }){
+    const hasBorder = props.hasBorder !== false;
+
+    return (
+        <img 
+            className={hasBorder ? styleAvatar.avatarWithBorder : styleAvatar.avatar} 
+            src={src} 
+        />
+    );
+}
