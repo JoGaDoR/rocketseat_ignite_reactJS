69bf4449bea16c90810e8ae3871f7e89ff4f955a
JoGaDoR <thiagojogador171@gmail.com>
2023-11-26 02:24:21 -0300

    feat: inserting comments declarative programming

---
+++ module03_react_engines/04_inserting_comments_declarative_programming/src/components/Avatar.jsx
@@ -0,0 +1,21 @@
+import styleAvatar from "./Avatar.module.css"
+
+// export function Avatar(props){
+//     const hasBorder = props.hasBorder !== false;
+
+//     return (
+//         <img 
+//             className={hasBorder ? styleAvatar.avatarWithBorder : styleAvatar.avatar} 
+//             src="props.src" 
+//         />
+//     );
+// }
+
+export function Avatar({ hasBorder = true, src }){
+    return (
+        <img 
+            className={hasBorder ? styleAvatar.avatarWithBorder : styleAvatar.avatar} 
+            src={src} 
+        />
+    );
+}
