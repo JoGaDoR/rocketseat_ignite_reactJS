314f828fe27c28d2f4c372c9c67020830dc13add
JoGaDoR <thiagojogador171@gmail.com>
2023-11-30 16:48:30 -0300

    feat: module understanding key

---
+++ module03_react_engines/05_understanding_key/src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { Avatar } from "./Avatar"
+import styleComment from "./Comment.module.css"
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+
+export function Comment({content}){
+    return(
+        <div className={styleComment.comment}>
+            <Avatar hasBorder={false} src="https://source.unsplash.com/random?blue" />
+
+            <div className={styleComment.commentBox}>
+                <div className={styleComment.commentContent}>
+                    <header>
+                        <div className={styleComment.authorAndTime}>
+                            <strong>Thiago Coelho</strong>
+                            <time title="11 de maio as 9:13" dateTime="2022-05-11">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p> {content} </p>
+
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
---
+++ module03_react_engines/05_understanding_key/src/components/Post.jsx
@@ -0,0 +1,97 @@
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { useState } from "react";
+
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
+import stylePost from "./Post.module.css";
+
+
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState(
+    [
+      "Primeiro post hein?!"
+    ]
+  )
+
+  const [newCommenttext, setNewCommentText] = useState('')
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "dd 'de' LLLL 'as' HH:mm'h'",
+    { locale: ptBR }
+  );
+
+  const publisedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
+  function handleCreateNewComment(){
+    event.preventDefault()
+
+    // const newCommenttext = event.target.comment.value
+
+    setComments([...comments, newCommenttext])
+
+    // event.target.comment.value = ''
+  }
+
+  function handleNewCommentChange(){
+    setNewCommentText(event.target.value)
+
+    setNewCommentText('')
+  }
+
+  return (
+    <article className={stylePost.post}>
+      <header>
+        <div className={stylePost.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={stylePost.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publisedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={stylePost.content}>
+        {
+          content.map(item => {
+            if (item.type === 'paragraph'){
+              return <p>{item.content}</p>
+            } else if (item.type === "link"){
+              return <p><a href="#">{item.content}</a></p>
+            }
+          })
+        }
+      </div>
+
+      <form onSubmit={handleCreateNewComment} className={stylePost.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={newCommenttext}
+          onChange={handleNewCommentChange}
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={stylePost.commentList}>
+        {
+          comments.map(comment => {
+           return <Comment content={comment} />
+          })
+        }
+      </div>
+    </article>
+  );
+}
