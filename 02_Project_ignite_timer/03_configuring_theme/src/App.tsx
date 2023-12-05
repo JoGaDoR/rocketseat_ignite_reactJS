b270e55ba74ccfde46699b3a9c37e6dc90ae5aa5
JoGaDoR <thiagojogador171@gmail.com>
2023-12-05 02:15:55 -0300

    feat: configuring theme

---
+++ 02_Project_ignite_timer/03_configuring_theme/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+    <> 
+      <Button variantColor="primary" />
+      <Button variantColor="secondary"/>
+      <Button variantColor="success"/>
+      <Button variantColor="danger"/>
+      <Button />
+    </>
+  )
+}
