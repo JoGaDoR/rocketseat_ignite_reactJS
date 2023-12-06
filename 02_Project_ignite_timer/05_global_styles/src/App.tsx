5a44b9bbf640a9eb6ca51790c75929462211c65f
jogador <thiagojogador171@gmail.com>
2023-12-06 01:52:16 -0300

    feat: global styles

---
+++ 02_Project_ignite_timer/05_global_styles/src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { deafultTheme } from "./styles/theme/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={deafultTheme}> 
+      <Button variantColor="primary" />
+      <Button variantColor="secondary"/>
+      <Button variantColor="success"/>
+      <Button variantColor="danger"/>
+      <Button />
+    </ThemeProvider>
+  )
+}
