4b5a008c9f67a358d4604dad503c0b231bdb544a
jogador <thiagojogador171@gmail.com>
2023-12-09 01:28:30 -0300

    feat: home page

---
+++ 03_pages_and_routes/04_home_page/src/pages/History.tsx
@@ -0,0 +1,11 @@
+// import { Header } from '../components/Header'
+
+export function History() {
+  return (
+    <div>
+      {/* <Header></Header> */}
+
+      <h1>HISTORY</h1>
+    </div>
+  )
+}
---
+++ 03_pages_and_routes/04_home_page/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+// import { Header } from '../components/Header'
+
+export function Home() {
+  return (
+    <div>
+      {/* <Header></Header> */}
+
+      <h1>home</h1>
+    </div>
+  )
+}
