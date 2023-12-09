41d2f8407b89b84983d9b5a93c2883042646c79e
jogador <thiagojogador171@gmail.com>
2023-12-09 01:44:21 -0300

    feat: inserting style container

---
+++ 03_pages_and_routes/04_home_page/src/pages/Home/styles.ts
@@ -0,0 +1,15 @@
+import styledHome from 'styled-components'
+
+export const HomeContainer = styledHome.main`
+
+
+`
+
+export const FormContainer = styledHome.div`
+
+`
+
+export const CountdownContainer = styledHome.div`
+
+
+`
