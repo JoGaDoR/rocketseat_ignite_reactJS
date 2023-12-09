c8579c017d0a39e6b68197eb77d8771eb58d9395
jogador <thiagojogador171@gmail.com>
2023-12-09 01:57:43 -0300

    feat: CountdownContainer style

--- 03_pages_and_routes/04_home_page/src/pages/Home/styles.ts
+++ 03_pages_and_routes/04_home_page/src/pages/Home/styles.ts
@@ -30,6 +30,18 @@ export const FormContainer = styledHome.div`
 `
 
 export const CountdownContainer = styledHome.div`
+    font-family: 'Roboto Mono', monospace;
+    font-size: 10rem;
+    line-height: 8rem;
+    color: ${(props) => props.theme['gray-100']}
 
+    display: flex;
+    gap: 1rem;
+
+    span{
+        background: ${(props) => props.theme['gray-700']};
+        padding: 2rem 1rem;
+        border-radius: 8px;
+    }
 
 `
