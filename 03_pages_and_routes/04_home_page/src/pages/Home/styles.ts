e3ebdf3a4371e91df998861b375eaf41c66eebca
jogador <thiagojogador171@gmail.com>
2023-12-09 01:47:28 -0300

    feat: HomeContainer style

--- 03_pages_and_routes/04_home_page/src/pages/Home/styles.ts
+++ 03_pages_and_routes/04_home_page/src/pages/Home/styles.ts
@@ -1,8 +1,20 @@
 import styledHome from 'styled-components'
 
 export const HomeContainer = styledHome.main`
+    // height: 100%;
+    flex: 1;
 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
+    form{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 3.5rem;
+    }
 `
 
 export const FormContainer = styledHome.div`
