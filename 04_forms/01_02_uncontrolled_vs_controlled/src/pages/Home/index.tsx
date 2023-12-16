78d628ecdcde5be9a010fa0742dbae13d3332a5e
jogador <thiagojogador171@gmail.com>
2023-12-16 02:32:47 -0300

    feat: enable button via input using uncontrolled

--- 04_forms/01_02_uncontrolled_vs_controlled/src/pages/Home/index.tsx
+++ 04_forms/01_02_uncontrolled_vs_controlled/src/pages/Home/index.tsx
@@ -8,17 +8,50 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
+import { useRef, useEffect } from 'react'
 
 export function Home() {
+  const taskInputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    const currentTaskInputRef = taskInputRef.current
+    function handleTaskInputChange() {
+      const isTaskInputFilled = taskInputRef.current?.value !== ''
+      const startCountdownButton = document.getElementById(
+        'start-countdown-button',
+      ) as HTMLButtonElement
+
+      if (startCountdownButton) {
+        startCountdownButton.disabled = !isTaskInputFilled
+      }
+    }
+
+    if (currentTaskInputRef) {
+      currentTaskInputRef.addEventListener('input', handleTaskInputChange)
+    }
+
+    return () => {
+      if (currentTaskInputRef) {
+        currentTaskInputRef.removeEventListener('input', handleTaskInputChange)
+      }
+    }
+  }, [])
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+  }
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
+            name="task"
             list="task-suggestions"
             placeholder="De um nome para o seu projeto"
+            ref={taskInputRef}
           />
 
           <datalist id="task-suggestions">
@@ -51,7 +84,7 @@ export function Home() {
           </div>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton id="start-countdown-button" type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
