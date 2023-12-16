6269a8d5344d146bdbe726ef9f01a6f82351e325
jogador <thiagojogador171@gmail.com>
2023-12-16 01:54:36 -0300

    Revert "feat: controlled vs uncrontolled"
    This reverts commit 48f2fed1882f7881cca7cfe047e9a395a35b5e6a.

--- 04_forms/01_controlled_vs_uncontrolled/src/@types/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components'
-import { deafultTheme } from '../styles/theme/default'
-
-type ThemeType = typeof deafultTheme
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
-}
