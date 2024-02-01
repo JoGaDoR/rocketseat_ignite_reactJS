7c85a42e24e08f1146fb705c1b63a64adcbf92ee
jogador <thiagojogador171@gmail.com>
2024-02-01 01:40:37 -0300

    feat: reducing the countdown

---
+++ 02_Project_ignite_timer/module_04_application_features/04_reducing_the_countdown/src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,16 @@
+import styledLayout from 'styled-components'
+import { DefaultTheme } from 'styled-components/dist/types'
+
+export const LayoutContainer = styledLayout.div`
+    max-width: 74rem;
+    height: calc(100vh - 10rem);
+    margin: 5rem auto;
+    padding: 2.5rem;
+
+    background: ${(props: DefaultTheme) => props.theme['gray-800']};
+    border-radius: 8px;
+
+    display: flex;
+    flex-direction: column;
+
+`
