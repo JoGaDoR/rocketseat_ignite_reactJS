4ac168ef602bebb09ae51dbadb0681685daf7cc0
jogador <thiagojogador171@gmail.com>
2024-02-02 23:07:03 -0300

    feat: disabled form input

--- 02_Project_ignite_timer/module_04_application_features/06_interrupt_the_cycle/src/pages/Home/index.tsx
+++ 02_Project_ignite_timer/module_04_application_features/06_interrupt_the_cycle/src/pages/Home/index.tsx
@@ -109,6 +109,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="De um nome para o seu projeto"
+            disabled={!!activeCycle}
             {...register('task')}
           />
 
@@ -127,6 +128,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            disabled={!!activeCycle}
             {...register('minutesAmount', { valueAsNumber: true })}
           />
 
