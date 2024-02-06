a1264e3404aeae5b9f07a5b180d4d85f00d7d09e
jogador <thiagojogador171@gmail.com>
2024-02-06 02:24:23 -0300

    feat: advanced context

--- 02_Project_ignite_timer/module_05_context_in_react/01_understanding_context/src/Home.tsx
+++ 02_Project_ignite_timer/module_05_context_in_react/01_understanding_context/src/Home.tsx
@@ -1,19 +1,25 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 // import { CountDown } from "./CountDown";
 // import { NewCycleForm } from "./NewCycleForm";
 
 //criar uma variavel com o contexto e colocar entre paranteses o valor inicial do contexto
 //recomendado iniciar sempre como objeto
-export const CyclesContext = createContext({
-    activeCycle: 1
-  })
+// const CyclesContext = createContext({
+//     activeCycle: 1
+//   })
+const CyclesContext = createContext({} as any)
 
   export function CountDown(){
     //usar a informação do CuclesContext usando o useCon
-    const { activeCycle } = useContext(CyclesContext)
+    const { activeCycle, setActiveCycle } = useContext(CyclesContext)
 
     return (
-        <h1>CountDown: {activeCycle}</h1>
+        <h1>
+            CountDown: {activeCycle}
+            <button onClick={() => setActiveCycle(2)}>
+                Alterar ciclo
+                </button>
+        </h1>
     )
 }
 
@@ -26,10 +32,18 @@ export function NewCycleForm(){
 }
 
 export function Home () {
+    //usar um estado para visualizar as alterações do context
+    //aqui o valor do activeCycle não sera mais o valor inicial, agora prevalece o do useState/Provider
+    const [activeCycle, setActiveCycle] = useState(0)
+
     return (
+        //o Provider do context serve como Props para que se possa compartilhar 
+        //informações desejadas apenas com os componentes filhos
+        <CyclesContext.Provider value={{activeCycle, setActiveCycle}}>
         <div>
             <CountDown/>
             <NewCycleForm/>
         </div>
+        </CyclesContext.Provider>
     )       
 }
